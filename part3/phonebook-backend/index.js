3080688648ede30295a233f45aac0178784916b8
Olga Shomarova <olga.shomarova@protonmail.com>
2025-06-20 08:00:19 +0300

    finish 3 part

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,61 +2,49 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/person')
-const mongoose = require('mongoose');
 const app = express()
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'))
-morgan.token('reqObj', (req)=> JSON.stringify(req.body))
+morgan.token('reqObj', (req) => JSON.stringify(req.body))
 app.use(morgan(':reqObj'))
 
-app.get('/api/persons', (req,res, next)=>{
-    Person
-    .find({})
-    .then(persons => {
-      res.json(persons)
-    })
-    .catch(error => next(error))
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  }).catch(error => next(error))
 })
 
-app.get('/info', (req, res, next)=> {
-  Person
-    .find({})
-    .then(persons=> {
-      res.send(`<p>Phonebook has info for ${persons.length} people<br />
+app.get('/info', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.send(`<p>Phonebook has info for ${persons.length} people<br />
         ${Date()}</p>`)
-    })
-    .catch(error => next(error))
+  }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res, next)=> {
-  Person
-  .findById(req.params.id)
-  .then(person => {
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
     if (person) {
       res.json(person)
     } else {
       res.status(404).end()
     }
-  })
-  .catch(error => next(error))
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndDelete(req.params.id)
-      .then(result=>{
-        res.status(204).end()
-  })
-      .catch(error=> next(error))
+  Person.findByIdAndDelete(req.params.id).then(() => {
+    res.status(204).end()
+  }).catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res, next) =>{
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
     return res.status(400).json({
-      error: 'content missing'
+      error: 'content missing',
     })
   }
 
@@ -64,29 +52,25 @@ app.post('/api/persons', (req, res, next) =>{
     name: body.name,
     number: body.number,
   })
-  person.save()
-    .then(savedPerson => {
-        res.json(savedPerson)
-    })
-      .catch(error => next(error))
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const {name, number} = req.body
+  const { name, number } = req.body
 
-  Person.findById(req.params.id)
-      .then(person => {
-        if(!person) {
-          res.status(404).end()
-        }
-        person.name = name;
-        person.number = number;
+  Person.findById(req.params.id).then(person => {
+    if (!person) {
+      res.status(404).end()
+    }
+    person.name = name
+    person.number = number
 
-        return person.save().then(updatedPerson => {
-          res.json(updatedPerson)
-        })
-      })
-      .catch(error => next(error))
+    return person.save().then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+  }).catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -14,28 +14,28 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-      name: String,
-      number: String
-    }
+  name: String,
+  number: String
+}
 )
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 5) {
- const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4]
-})
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-person.save().then(result => {
-  console.log(`added ${result.name} ${result.number} to phonebook`)
-  mongoose.connection.close()
-})
+  person.save().then(result => {
+    console.log(`added ${result.name} ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 3) {
   Person
     .find({})
     .then(persons => {
       persons.forEach(p => console.log(p))
       mongoose.connection.close()
-  })
+    })
 }
